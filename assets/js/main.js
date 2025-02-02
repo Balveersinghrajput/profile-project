475b72b4402fb47ed407c72608d1ac8e4564f59c
Balveersingh Rajput <balveersinghrajput2004@gmail.com>
2025-02-02 07:56:08 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,34 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/* Menu show */
+if(navToggle){
+   navToggle.addEventListener('click', () =>{
+      navMenu.classList.add('show-menu')
+   })
+}
+
+/* Menu hidden */
+if(navClose){
+   navClose.addEventListener('click', () =>{
+      navMenu.classList.remove('show-menu')
+   })
+}
+
+// section -3 ------------------------------------------------>
+
+document.querySelectorAll('.accordion-toggle').forEach(button => {
+   button.addEventListener('click', () => {
+       const content = button.nextElementSibling;
+
+       // Toggle the visibility of the content
+       if (content.style.display === 'block') {
+           content.style.display = 'none';
+       } else {
+           content.style.display = 'block';
+       }
+   });
+});
+
